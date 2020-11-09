99966dcd6ed93654db9b5b1a587ea6baf23880fc
ConOLeary <conolaoghaire@gmail.com>
2020-11-09 17:59:39 +0000

    initial bits

---
+++ server.js
@@ -0,0 +1,37 @@
+//alert("ooooo");
+
+const app = new Vue({
+    el: '#main',
+   
+    data: {
+        result: " ",
+        responseAvailable: false,
+        apiKey: '3e2d927d4f28b456c6bc662f34350957'
+    },
+    methods: {
+        fetchAPIData( ) { 
+            this.responseAvailable = false;
+            fetch("https://jokes-database.p.rapidapi.com/", {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-host": "jokes-database.p.rapidapi.com",
+                    "x-rapidapi-key": this.apiKey
+                }
+            })
+            .then(response => { 
+                if(response.ok){
+                    return response.json()    
+                } else{
+                    alert("Server returned " + response.status + " : " + response.statusText);
+                }                
+            })
+            .then(response => {
+                this.result = response.body; 
+                this.responseAvailable = true;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    }
+})
